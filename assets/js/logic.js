7ac9305dfc5e0b579a686abcae27cc5889407680
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-06-29 16:13:48 -0400

    Set up junction in JS to break horizontal scroll at a certain page size

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,27 @@
-$("#page-wrap").wrapInner("<table cellspacing='30'><tr>");
-$(".post").wrap("<td></td>");
-$("body").mousewheel(function(event, delta) {
-  this.scrollLeft -= delta * 30;
-  event.preventDefault();
-});
+
+leftToRightScroll();
+
+function leftToRightScroll() {
+  $("#page-wrap").wrapInner("<table cellspacing='30'><tr>");
+  $(".post").wrap("<td></td>");
+  $("body").mousewheel(function (event, delta) {
+    this.scrollLeft -= delta * 30;
+    event.preventDefault();
+    if ($(window).width() < 800) {
+      breakScroll() 
+    }
+
+
+  });
+}
+
+function breakScroll() {
+  $("#page-wrap").wrapInner("<table cellspacing='30'><tr>");
+  $(".post").wrap("<td></td>");
+  $("body").mousewheel(function (event, delta) {
+    this.scrollLeft -= delta * 30;
+    event.preventDefault();
+    break;
+
+  });
+}
